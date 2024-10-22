757f606e87caf771010a15e92ff8a0cfba6dd197
Kvitoslava <kyarish@kse.org.ua>
2024-10-22 23:16:13 +0300

    first academ

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def get_students():
     pass
-def academ_integrity():
-    pass
+def academ_integrity(students:dict):
+    result = {}
+    for name, marks in students.items():
+        if -2<marks['marks'] - marks['final'] < 2:
+            result[name] = 'good'
+        else:
+            result[name] = 'bad'
+    return result
+
 
 def program():
     pass
